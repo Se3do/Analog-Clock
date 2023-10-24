b23a30a088ee6d0899289cb925be10ac28b52efe
Mohammed Said <114269947+Se3do@users.noreply.github.com>
2023-10-25 01:32:22 +0300

    Add files via upload

---
+++ JavaClock/src/application/Controller.java
@@ -0,0 +1,12 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+
+public class Controller{
+	
+	}
